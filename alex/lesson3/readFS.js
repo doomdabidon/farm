7661ef53dcd213b0403c1e3aae2475b8c3c373cf
AliaksandraBayanouskaya <alex.bayanouskaya@gmail.com>
2025-02-17 17:43:50 +0100

    Lesson stream

--- alex/lesson3/readFS.js
+++ alex/lesson3/readFS.js
@@ -1,8 +1,17 @@
 const fs = require('fs')
-process.on("message", () => {
-    fs.readFile('./alex/lesson3/1.txt', { encoding: 'utf-8' }, (error, con) => {
-        process.send(con.toString());
-        process.exit();
-    })
-});
+process.on('message', () => {
+    const stream = fs.createReadStream('./alex/lesson3/1.txt', { encoding: 'utf-8' });
+    console.log("Starting file reading.")
+    stream
+        .on('data', chunk => {
+            process.send(chunk.toString());
+            process.exit();
+        })
+        .on('end', () => {
+            console.log('Finished file reading.')
+        })
+        .on('error', error => {
+            console.log(`Error during file reading ${error}.`);
+        });
+})
 
