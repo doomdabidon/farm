72a3cf0d8399e6c84ed12d64e8e8409cd660b6dc
AliaksandraBayanouskaya <alex.bayanouskaya@gmail.com>
2025-03-03 14:54:41 +0100

    Lesson 5

--- alex/lesson5/authorValidator.js
+++ alex/lesson5/authorValidator.js
@@ -6,16 +6,4 @@ const authorNameValidator = z.object({
         .max(60, "The author name cannot be more then 60 symbols")
 });
 
-const authorBodyValidator = z.object({
-    id: z.string()
-        .min(1, "Wrong Id, min is 1")
-        .max(2000, "Wrong Id, max is 2000"),
-    name: z.string()
-        .min(3, "The author name cannot be less 3 symbols")
-        .max(60, "The author name cannot be more then 60 symbols"),
-    authorId: z.string()
-        .min(1, "Wrong Id, min is 1")
-        .max(2000, "Wrong Id, max is 2000")
-})
-
 module.exports = { authorBodyValidator, authorNameValidator }
